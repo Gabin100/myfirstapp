1897b37c4673f7c3bd1dfa33158efb631259cd44
Jean Gabin <ij.gabin123@gmail.com>
2019-07-08 23:20:39 +0200

    landing display leads from route and after subm

--- controllers/landing.js
+++ controllers/landing.js
@@ -8,6 +8,13 @@ exports.get_landing = function(req, res, next) {
     return models.lead.create({
       email: req.body.lead_email
     }).then(lead => {
-      res.redirect('/');
+      res.redirect('/leads');
     });
+  };
+
+  exports.show_leads = function(req, res, next) {
+    models.lead.findAll().then(leads => {
+      res.render('landing', { title: 'Express' , leads: leads});
+    })
+    
   };
