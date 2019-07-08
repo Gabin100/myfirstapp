48ad6cca33217b988ba1fc08c0431298ebe3a0cc
Jean Gabin <ij.gabin123@gmail.com>
2019-07-08 22:55:24 +0200

    save leads and landing page correct in www

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,8 +1,13 @@
+const models = require('../models');
+
 exports.get_landing = function(req, res, next) {
     res.render('landing', { title: 'Express' });
   };
 
   exports.submit_lead = function(req, res, next) {
-    console.log("Lead E-mail: ", req.body.lead_email);
-    res.redirect('/');
+    return models.lead.create({
+      email: req.body.lead_email
+    }).then(lead => {
+      res.redirect('/');
+    });
   };
