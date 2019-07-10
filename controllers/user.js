dccd15236fe2c4e056cd457ec0ac12b4aba9131e
Jean Gabin <ij.gabin123@gmail.com>
2019-07-10 19:07:59 +0200

    user

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,10 @@ const myPassport = require('../passport_setup');
 
 let flash = require('connect-flash');
 
+const generateHash = function (password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+}
+
 exports.show_login = function(req, res, next){
     console.log('login');
     res.render('user/login', {title: 'Login', formData: {}, errors: {}});
@@ -13,12 +17,11 @@ exports.show_login = function(req, res, next){
 exports.show_signup = function(req, res, next){
     res.render('user/signup', {title: 'Signup', formData: {}, errors: {}});
 }
-const generateHash = function (password){
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-}
+
 exports.login = function(req, res, next){
     
 }
+
 exports.signup = function(req, res, next){
     console.log(req.body.Password1 + " " + req.body.Email1);
     const newUser = models.User.build({
